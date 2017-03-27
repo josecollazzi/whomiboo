cd90f2a63c8c1cfb9746f0c3b50f2a871066858d
jose <josecollazzi@gmail.com>
2017-03-27 22:48:28 +0100

    added types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import JSONTree from 'react-json-tree';
+{/**import JSONTree from 'react-json-tree';**/}
 import { Button, Panel } from 'react-bootstrap';
 var fileDownload = require('react-file-download');
 
@@ -11,43 +11,26 @@ import metadataRequest from './structures/MetadataRequest.json';
 import metadataResponse from './structures/MetadataResponse.json';
 import configurationValue from './structures/ConfigurationValue.json';
 
-const theme = {
-  scheme: 'monokai',
-  author: 'wimer hazenberg (http://www.monokai.nl)',
-  base00: '#272822',
-  base01: '#383830',
-  base02: '#49483e',
-  base03: '#75715e',
-  base04: '#a59f85',
-  base05: '#f8f8f2',
-  base06: '#f5f4f1',
-  base07: '#f9f8f5',
-  base08: '#f92672',
-  base09: '#fd971f',
-  base0A: '#f4bf75',
-  base0B: '#a6e22e',
-  base0C: '#a1efe4',
-  base0D: '#66d9ef',
-  base0E: '#ae81ff',
-  base0F: '#cc6633'
-};
-
 class MetadataViewer extends Component {
   constructor(props) {
     super(props);
   }
 
   render  () {
-    const listItems = this.props.metadata.configurationValues.map((value) =><p>{value.developerName}</p>);
-    const listActions = this.props.metadata.actions.map((value) =><p>{value.developerName}</p>);
+    const listConfig = this.props.metadata.configurationValues.map((value) => <p>{value.developerName}</p>);
+    const listActions = this.props.metadata.actions.map((value) => <p>{value.developerName}</p>);
+    const listTypes = this.props.metadata.install.typeElements.map((value) => <p>{value.developerName}</p>);
 
     return (
       <div>
       <Panel header="List of configurationValues" bsStyle="success">
-        {listItems}
+        {listConfig}
       </Panel>
       <Panel header="List of actions" bsStyle="success">
         {listActions}
+      </Panel>
+      <Panel header="List of types" bsStyle="success">
+        {listTypes}
       </Panel>
       </div>
     );
